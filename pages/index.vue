7d115f5ee4abbde9f3de163040a878efe5a72e64
Arnór Bogason <arnorbogason@Megabyte.local>
2020-03-07 10:39:18 +0000

    add route

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
         <b-row class="justify-content-center">
             <b-col lg="8" class="mb-5">
                 <p>Ég heiti Arnór og ég er sjálfstætt starfandi vefhönnuður og vefforritari. Ég tek að mér ýmisskonar verkefni og itae arcu molestie, finibus risus a, imperdiet quam. Vivamus erat metus, consequat ut viverra eget, venenatis sollicitudin orci. Vestibulum interdum commodo sem ut viverra. Praesent dolor elit, vulputate eget nibh a, tincidunt congue dolor. Nulla ac efficitur diam, eu egestas nulla. Proin elit neque, ultrices nec neque lacinia, vehicula suscipit urna. Etiam vel pretium augue, sit amet pharetra lectus.</p>
+                <nuxt-link to="verkefni/hsorka">HS Orka</nuxt-link>
             </b-col>
         </b-row>
         <b-row>
