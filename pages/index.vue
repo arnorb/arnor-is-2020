3b2a4cec5ba84ab5206894239810153d90788032
Arnór Bogason <arnorbogason@Megabyte.local>
2020-04-01 18:42:26 +0000

    bla

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <b-container>
         <b-row class="justify-content-center">
             <b-col lg="8" class="mb-3">
-                <p>Ég heiti Arnór og ég er sjálfstætt starfandi vefhönnuður og vefforritari. Þú getur <nuxt-link to="/verkefni">skoðað verkefni sem ég hef unnið</nuxt-link> að en líka <nuxt-link to="/">lesið meira um mig</nuxt-link>. Ef þú vilt <nuxt-link to="/">hafa samband</nuxt-link> þá svara ég þér fljótt.</p>
+                <p>Ég heiti Arnór og ég er sjálfstætt starfandi vefhönnuður og vefforritari. Þú getur <nuxt-link to="/verkefni">skoðað verkefni sem ég hef unnið</nuxt-link> að en líka <nuxt-link to="/um-mig">lesið meira um mig</nuxt-link>. Ef þú vilt <nuxt-link to="/hafa-samband">hafa samband</nuxt-link> þá svara ég þér fljótt.</p>
             </b-col>
         </b-row>
         <b-row>
